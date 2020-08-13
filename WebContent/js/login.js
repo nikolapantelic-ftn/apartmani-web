95bf5248dc18c962a6166b4ce1c720052be1bc6a
stojke13 <stojakovic_djordje@yahoo.com>
2020-08-13 11:36:48 +0200

    Login implementiran

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,6 +1,43 @@
+
 var app = new Vue({
-	el: '#user-form',
-	data: {
-		register: false
+
+	el:'#login',
+	data:{
+		username:null,
+		password:null,
+		errors:[],
+		user:null,
+		info:null,
+		register: false,
+		
 	},
-})
+	methods:{
+		checkForm: function(e){
+			this.errors = [];
+			if(!this.username){
+				this.errors.push("Korisnicko ime je obavezno!");
+			}
+			if(!this.password){
+				this.errors.push("Lozinka je obavezna!");
+			}
+			e.preventDefault();
+			axios
+			.post('rest/login',{
+				username:this.username,
+				password:this.password
+			})
+			.then(response=>{
+				window.location.href = '/apartmani-web';
+				return true;
+			})
+			.catch(e=>{
+				this.info=e.response.status;
+				if(e.response.status==400){
+					this.errors.push("Pogresno korisnicko ime/lozinka!");
+				}
+			})
+			
+		}
+	}
+}
+)
