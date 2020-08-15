ed4dac0f0a14612e3650963a8397252b76eacff6
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2020-08-15 18:11:31 +0200

    Booking form podesen

--- WebContent/js/booking-form.js
+++ WebContent/js/booking-form.js
@@ -10,60 +10,58 @@ Vue.component("booking-form",{
 	},
 	template:
 	`
-		<div class="section-center">
-			<div class="container">
+		<div class="booking-form">
+			<form>
 				<div class="row">
-					<div class="booking-form">
-						<form>
-							<div class="col-md-8">
-								<div class="form-group">
-									<input class="form-control" v-model="search" type="text" placeholder="Unesite destinaciju">
-									<span class="form-label">Destinacija</span>
-								</div>
-							</div>
-							<div class="col-md-2">
-								<div class="form-group">
-									<select class="form-control" v-model="guests">
-										<option>1</option>
-										<option>2</option>
-										<option>3</option>
-									</select>
-									<span class="select-arrow"></span>
-									<span class="form-label">Gosti</span>
-								</div>
-							</div>
-							<div class="col-md-2">
-								<div class="form-group">
-									<select class="form-control" v-model='children'>
-										<option>0</option>
-										<option>1</option>
-										<option>2</option>
-									</select>
-									<span class="select-arrow"></span>
-									<span class="form-label">Deca</span>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-group">
-									<input class="form-control" type="date" required v-model='startDate'>
-									<span class="form-label">Datum prijave</span>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-group">
-									<input class="form-control" type="date" required v-model="endDate">
-									<span class="form-label">Datum odjave</span>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="form-btn">
-									<button class="submit-btn">Proveri raspolozivost</button>
-								</div>
-							</div>
-						</form>
+					<div class="col-md-8">
+						<div class="form-group">
+							<input class="form-control" v-model="search" type="text" placeholder="Unesite destinaciju">
+							<span class="form-label">Destinacija</span>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<select class="form-control" v-model="guests">
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+							</select>
+							<span class="select-arrow"></span>
+							<span class="form-label">Gosti</span>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<select class="form-control" v-model='children'>
+								<option>0</option>
+								<option>1</option>
+								<option>2</option>
+							</select>
+							<span class="select-arrow"></span>
+							<span class="form-label">Deca</span>
+						</div>
 					</div>
 				</div>
-			</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<input class="form-control" type="date" required v-model='startDate'>
+							<span class="form-label">Datum prijave</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="form-group">
+							<input class="form-control" type="date" required v-model="endDate">
+							<span class="form-label">Datum odjave</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="form-btn">
+							<button class="submit-btn">Proveri raspolozivost</button>
+						</div>
+					</div>
+				</div>
+			</form>
 		</div>
 	`	
 	
