199b6a7ed97fe68fd58520b46d2a125d24e0a97c
stojke13 <stojakovic_djordje@yahoo.com>
2020-08-18 16:49:25 +0200

    Booking form submit redirect dodat

--- WebContent/js/booking-form.js
+++ WebContent/js/booking-form.js
@@ -2,10 +2,15 @@ Vue.component("booking-form",{
 	data:function(){
 		return{
 			guests:1,
-			children:0,
+			rooms:0,
 			startDate:null,
 			endDate:null,
-			search:""
+			location:""
+		}
+	},
+	methods:{
+		searchApartments:function(){
+			window.location.href = '/apartmani-web/#/search?location='+this.location+'&rooms='+this.rooms+'&guests='+this.guests+'&sdate='+this.startDate+'&edate'+this.endDate;
 		}
 	},
 	template:
@@ -15,7 +20,7 @@ Vue.component("booking-form",{
 				<div class="row">
 					<div class="col-md-8">
 						<div class="form-group">
-							<input class="form-control" v-model="search" type="text" placeholder="Unesite destinaciju">
+							<input class="form-control" v-model="location" type="text" placeholder="Unesite destinaciju">
 							<span class="form-label">Destinacija</span>
 						</div>
 					</div>
@@ -32,13 +37,13 @@ Vue.component("booking-form",{
 					</div>
 					<div class="col-md-2">
 						<div class="form-group">
-							<select class="form-control" v-model='children'>
+							<select class="form-control" v-model='rooms'>
 								<option>0</option>
 								<option>1</option>
 								<option>2</option>
 							</select>
 							<span class="select-arrow"></span>
-							<span class="form-label">Deca</span>
+							<span class="form-label">Sobe</span>
 						</div>
 					</div>
 				</div>
@@ -57,7 +62,7 @@ Vue.component("booking-form",{
 					</div>
 					<div class="col-md-4">
 						<div class="form-btn">
-							<button class="submit-btn">Proveri raspolozivost</button>
+							<button v-on:click="searchApartments" class="submit-btn">Proveri raspolozivost</button>
 						</div>
 					</div>
 				</div>
