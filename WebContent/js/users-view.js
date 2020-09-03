c6847ac040518f3c950e38210045c330205bdade
stojke13 <stojakovic_djordje@yahoo.com>
2020-09-04 00:41:51 +0200

    Pretraga korisnika

--- WebContent/js/users-view.js
+++ WebContent/js/users-view.js
@@ -3,6 +3,7 @@ Vue.component('users-view',{
 		return{
 			currentUser:null,
 			users:[],
+			search:''
 		}
 	},
 	mounted(){
@@ -10,13 +11,27 @@ Vue.component('users-view',{
 		.get('rest/users')
 		.then(response => (this.users = response.data))
 	},
+	computed: {
+    filteredUsers() {
+      return this.users.filter(user => {
+       if(user.firstName.toLowerCase().match(this.search.toLowerCase())||user.lastName.toLowerCase().match(this.search.toLowerCase())|| user.username.toLowerCase().match(this.search.toLowerCase()))
+		return user;
+      })
+    }
+	},
 	template:
   `
   
 	<div class="container">
 	<div class="row">
+		 <div class="search-wrapper">
+			<label>Pretraga korisnika:</label>
+  		    <input type="text" v-model="search" placeholder="Search "/>
+  		</div>
+	</div>
+	<div class="row">
 	
-      <div class="col-lg-3 col-md-4 col-sm-6" v-for="user in users">
+      <div class="col-lg-3 col-md-4 col-sm-6" v-for="user in filteredUsers">
         <div class="panel panel-default userlist">
         
           <div class="panel-body text-center">
