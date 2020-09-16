d7b7b63eeafbefe01639a33b055f6c18b471593b
nikolapantelic-ftn <nikola.pantelic@uns.ac.rs>
2020-09-16 15:30:00 +0200

    Validacija izmene profila

--- WebContent/js/profile-view.js
+++ WebContent/js/profile-view.js
@@ -8,7 +8,11 @@ Vue.component('profile-view', {
 			password: '',
 			repeatedPassword: '',
 			editMode: false,
-			errors: []
+			hasErrors: false,
+			nameError: false,
+			lastNameError: false,
+			passwordError: false,
+			repeatedPasswordError: false
 		}
 	},
 	methods: {
@@ -16,7 +20,7 @@ Vue.component('profile-view', {
 			this.editMode = true;
 		},
 		cancel: function () {
-			this.errors = [];
+			this.clearErrors();
 			this.editMode = false;
 			this.firstName = this.user.firstName;
 			this.lastName = this.user.lastName;
@@ -25,6 +29,8 @@ Vue.component('profile-view', {
 			this.repeatedPassword = '';
 		},
 		submit: function () {
+			this.checkForm();
+			if (this.hasErrors) return;
 			this.user.firstName = this.firstName;
 			this.user.lastName = this.lastName;
 			this.user.gender = this.gender;
@@ -36,7 +42,7 @@ Vue.component('profile-view', {
 			axios
 				.post('rest/users' + servicePath, this.user)
 				.then(response => {
-					this.editMode = false;
+					this.editMode = false;this.hasErrors = true;
 					alert("Uspesno izmenjen profil.");
 					})
 				.catch(error => {
@@ -45,12 +51,8 @@ Vue.component('profile-view', {
 				});
 		},
 		changePassword: function () {
-			this.errors = [];
-			if (this.password.length == 0) return;
-			if (this.password != this.repeatedPassword) {
-				this.errors.push('Lozinke se ne poklapaju!')
-				return;
-			}
+			this.checkPasswordForm();
+			if (this.hasErrors) return;
 			this.user.password = this.password;
 			let servicePath = '/guest-profile';
 			if (this.user.role == 'Host') servicePath = '/host-profile';
@@ -67,6 +69,35 @@ Vue.component('profile-view', {
 				});
 			this.password = '';
 			this.repeatedPassword = '';
+		},
+		checkForm: function () {
+			this.clearErrors();
+			if (!this.firstName) {
+				this.hasErrors = true;
+				this.nameError = true;
+			}
+			if (!this.lastName) {
+				this.hasErrors = true;
+				this.lastNameError = true;
+			}
+		},
+		checkPasswordForm: function () {
+			this.clearErrors();
+			if (!this.password) {
+				this.hasErrors = true;
+				this.passwordError = true;
+			}
+			if (this.repeatedPassword != this.password) {
+				this.hasErrors = true;
+				this.repeatedPasswordError = true;
+			}
+		},
+		clearErrors: function () {
+			this.hasErrors = false;
+			this.nameError = false;
+			this.lastNameError = false;
+			this.passwordError = false;
+			this.repeatedPasswordError = false;
 		}
 	},
 	template: 
@@ -76,32 +107,32 @@ Vue.component('profile-view', {
 				<div class="form-row">
 					<div class="form-group col-md-5">
 						<label for="first-name">Ime:</label>
-						<input type="text" class="form-control" id="first-name" v-model="firstName" v-if="editMode">
-						<input type="text" class="form-control" id="first-name" v-model="firstName" disabled v-else>
+						<input type="text" class="form-control" v-bind:class="{ 'is-invalid': nameError }" id="first-name" v-model="firstName" :disabled="editMode == false">
+						<div class="invalid-feedback" v-if="nameError">
+		    				Ime je obavezno.
+		    			</div>
 					</div>
 					<div class="form-group col-md-5">
 						<label for="last-name">Prezime:</label>
-						<input type="text" class="form-control" id="last-name" v-model="lastName" v-if="editMode">
-						<input type="text" class="form-control" id="last-name" v-model="lastName" disabled v-else>
+						<input type="text" class="form-control" v-bind:class="{ 'is-invalid': lastNameError }" id="last-name" v-model="lastName" :disabled="editMode == false">
+						<div class="invalid-feedback" v-if="lastNameError">
+		    				Prezime je obavezno.
+		    			</div>
 					</div>
 				</div>
 				<div class="form-row">
 					<div class="form-group">
 						<label for="gender">Pol:</label>
-			    		<div id="gender" v-if="editMode">
+			    		<div id="gender">
 			    			<div class="form-check form-check-inline">
-					    		<input class="form-check-input" v-model="gender" type="radio" id="male" name="gender" value="Male">
+					    		<input class="form-check-input" v-model="gender" type="radio" id="male" name="gender" value="Male" :disabled="editMode == false">
 				                <label class="form-check-label" for="male">Muški</label>
 			                </div>
 			                <div class="form-check form-check-inline">
-				                <input class="form-check-input" v-model="gender" type="radio" id="female" name="gender" value="Female">
+				                <input class="form-check-input" v-model="gender" type="radio" id="female" name="gender" value="Female" :disabled="editMode == false">
 				                <label class="form-check-label" for="female">Ženski</label>
 			                </div>
 		                </div>
-						<div class="ml-2" id="gender" v-else>
-							<div v-if="gender == 'Male'">Muski</div>
-							<div v-if="gender == 'Female'">Zenski</div>
-						</div>
 					</div>
 				</div>
 				<button class="btn btn-primary" @click="edit" v-if="!editMode">Izmeni profil</button>
@@ -112,15 +143,20 @@ Vue.component('profile-view', {
 				<div class="form-row mt-5" v-if="editMode">
 					<div class="form-group col-md-5">
 						<label for="password">Nova lozinka:</label>
-						<input type="password" class="form-control" id="password" v-model="password" v-if="editMode">
+						<input type="password" class="form-control" v-bind:class="{ 'is-invalid': passwordError }" id="password" v-model="password" v-if="editMode">
+						<div class="invalid-feedback" v-if="passwordError">
+		    				Lozinka ne sme biti prazna.
+		    			</div>
 					</div>
 					<div class="form-group col-md-5">
 						<label for="repeated-password">Ponovljena lozinka:</label>
-						<input type="password" class="form-control" id="repeated-password" v-model="repeatedPassword" v-if="editMode">
+						<input type="password" class="form-control" v-bind:class="{ 'is-invalid': repeatedPasswordError }" id="repeated-password" v-model="repeatedPassword" v-if="editMode">
+						<div class="invalid-feedback" v-if="repeatedPasswordError">
+		    				Lozinke se ne poklapaju.
+		    			</div>
 					</div>
 				</div>
 				<button class="btn btn-warning" v-if="editMode" @click="changePassword">Izmeni lozinku</button>
-				<div v-if="errors.length">{{ errors[0] }}</div>
 			</div>
 		`
 })
